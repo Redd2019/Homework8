586da9128f32fda30e9f0540bc305861e86d47b4
Redd2019 <anton-mikerin@yandex.ru>
2023-01-29 20:41:45 +0300

    Выполнено задание 1

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        {
+            int[] numbers = new int[3];
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+            //
+            double[] numbers1 = {1.57, 7.654, 9.986};
+            //
+            int[] lenght = new int[5];
+            lenght[0] = 111;
+            lenght[1] = 112;
+            lenght[2] = 113;
+            lenght[3] = 114;
+            lenght[4] = 115;
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+    }
+
+
+    }
